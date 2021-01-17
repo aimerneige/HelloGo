8494040473d7af20df2222fd9c7fcf850631ce52
AimerNeige <aimer.neige.soft@gmail.com>
2021-01-17 17:44:47 +0800

    update syntax

---
+++ syntax/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"aimerneige.com/HelloGo/syntax/pkg1"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+	fmt.Println("https://aimerneige.com/")
+	pkg1.Foo()
+	// pkg1.bar()
+}
---
+++ syntax/pkg1/package01.go
@@ -0,0 +1,12 @@
+package pkg1
+
+import "fmt"
+
+// Foo print `Foo` at terminal
+func Foo() {
+	fmt.Println("Foo")
+}
+
+func bar() {
+	fmt.Println("Bar")
+}
